2290adf00067111a3db011f843467347aaf126a6
progdev8201 <56039386+progdev8201@users.noreply.github.com>
2020-09-01 19:59:35 -0400

    trying websocket

---
+++ src/main/java/com/model/entity/Greeting.java
@@ -0,0 +1,16 @@
+package com.model.entity;
+
+public class Greeting {
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
