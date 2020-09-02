6f7f38d42b9c416ebfc37139088e73070b605011
momolakeit <56810449+momolakeit@users.noreply.github.com>
2020-09-01 22:28:57 -0400

    rate service

--- src/main/java/com/service/RatingService.java
+++ src/main/java/com/service/RatingService.java
@@ -4,22 +4,38 @@ import com.mapping.RateToRateDTOImpl;
 import com.model.dto.RateDTO;
 import com.model.entity.Product;
 import com.model.entity.Rate;
+import com.repository.ProductRepository;
 import com.repository.RateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+@Service
 public class RatingService {
 
     private RateRepository rateRepository;
 
+    private ProductRepository productRepository;
+
     @Autowired
-    public RatingService(RateRepository rateRepository) {
+    public RatingService(RateRepository rateRepository,ProductRepository productRepository) {
         this.rateRepository = rateRepository;
+        this.productRepository = productRepository;
     }
 
-    public RateDTO createRate(Rate rate){
+    public RateDTO createRate(Rate rate,Product product){
+        rate = rateRepository.save(rate);
+        if(Objects.isNull(product.getRates())){
+            List<Rate> rates = new ArrayList<>();
+            rates.add(rate);
+            product.setRates(rates);
+            productRepository.save(product);
+
+        }
         return RateToRateDTOImpl.instance.convert(rateRepository.save(rate));
     }
     public double findAverageRate(Product product){
---
+++ src/test/java/com/controller/RateControllerTest.java
@@ -0,0 +1,61 @@
+package com.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.model.dto.BillDTO;
+import com.model.dto.MenuDTO;
+import com.model.dto.RateDTO;
+import com.model.entity.Product;
+import com.model.entity.Rate;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+class RateControllerTest {
+
+    @Autowired
+    RateController rateController;
+    @Test
+    public void testCreateMakeAjouterSpecial() throws Exception {
+        MockMvc mvc = initMockMvc();
+        LinkedMultiValueMap<String,String> requestParams = new LinkedMultiValueMap<>();
+
+        Rate rate = new Rate();
+        rate.setRate(5);
+        rate.setRaterUsername("killua");
+        Product product = new Product();
+        product.setName("le steak chico");
+        product.setPrix(29.99);
+        ObjectMapper objectMapper =new ObjectMapper();
+
+
+        JSONObject sendObj = new JSONObject();
+        sendObj.put("rate",objectMapper.writeValueAsString(rate));
+        sendObj.put("product",objectMapper.writeValueAsString(rate));
+
+        MvcResult result= mvc.perform(MockMvcRequestBuilders.post(   "/rate/createRate").
+                content(sendObj.toString()).
+                contentType(MediaType.APPLICATION_JSON).
+                accept(MediaType.APPLICATION_JSON)).
+                andExpect(status().isOk()).
+                andReturn();
+
+        RateDTO reponse = new ObjectMapper().readValue(result.getResponse().getContentAsString(), RateDTO.class);
+        assertNotNull(reponse);
+    }
+
+    private MockMvc initMockMvc(){
+        return MockMvcBuilders.standaloneSetup(rateController).build();
+    }
+
+}
