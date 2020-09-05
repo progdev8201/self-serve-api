ecdb081e6fb93a544bcdcafde1a4b023152e6132
progdev8201 <56039386+progdev8201@users.noreply.github.com>
2020-09-05 17:42:45 -0400

    Update EmailService.java

--- src/main/java/com/service/EmailService.java
+++ src/main/java/com/service/EmailService.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
@@ -16,13 +20,22 @@ public class EmailService {
     private String fabMoeEmail;
 
 
-    public void sendSimpleMessage(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(to);
-        message.setTo(fabMoeEmail);
-        message.setSubject(subject);
-        message.setText(text);
-        javaMailSender.send(message);
-        System.out.println("EMAIL WAS SENT SUCCESSFULLY!");
+    public boolean sendSimpleMessage(String ownerEmail, String subject, String text) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+        text = "<h3>" + text + "</h3><p>From: " + ownerEmail + "<p>";
+
+        try {
+            helper.setText(text,true);
+            helper.setFrom(ownerEmail);
+            helper.setTo(fabMoeEmail);
+            helper.setSubject(subject);
+            javaMailSender.send(mimeMessage);
+            System.out.println("EMAIL WAS SENT SUCCESSFULLY!");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 }
