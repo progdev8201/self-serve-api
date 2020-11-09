fb7e9fd1035b3961ae347b2b0521aac8af5f0bc6
momolakeit <56810449+momolakeit@users.noreply.github.com>
2020-11-09 06:55:16 -0500

    set dynamic account Id

---
+++ src/main/java/com/model/dto/StripeAccountIdDTO.java
@@ -0,0 +1,13 @@
+package com.model.dto;
+
+public class StripeAccountIdDTO {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
