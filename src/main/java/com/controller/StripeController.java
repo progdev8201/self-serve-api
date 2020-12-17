523a4cf320f603d1c2cbec955b34c955ec724566
momolakeit <56810449+momolakeit@users.noreply.github.com>
2020-12-16 21:00:48 -0500

    mettre les preAuth sur stripe controller

--- src/main/java/com/controller/StripeController.java
+++ src/main/java/com/controller/StripeController.java
@@ -11,6 +11,7 @@ import com.stripe.exception.StripeException;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -24,26 +25,28 @@ public class StripeController {
     @Autowired
     StripeService stripeService;
 
-
+    @PreAuthorize("hasAuthority('ROLE_OWNER')")
     @PostMapping("/createStripeAcccount")
     public ResponseEntity<StripeCreateAccountUrlDTO> createStripeAccount(@RequestBody Map<String, String> json) throws JsonProcessingException, StripeException {
         return ResponseEntity.ok(stripeService.createStripeAccount(json.get("username")));
     }
 
+    @PreAuthorize("hasAuthority('ROLE_OWNER')")
     @PostMapping("/saveAccountId")
     public ResponseEntity<?> saveOwnerAccountId(@RequestBody Map<String, String> json){
         String username= json.get("username");
         stripeService.saveStripeAccountId(username);
         return ResponseEntity.ok().build();
-
     }
+
+    @PreAuthorize("hasAnyAuthority('ROLE_GUEST','ROLE_CLIENT')")
     @PostMapping("/getAccountId")
     public ResponseEntity<StripeAccountIdDTO> getStripeAccountId(@RequestBody Map<String,String> json) throws JsonProcessingException {
         Long menuId = new ObjectMapper().readValue(json.get("restaurantId"),Long.class);
         return ResponseEntity.ok(stripeService.getAccountId(menuId));
     }
 
-
+    @PreAuthorize("hasAnyAuthority('ROLE_GUEST','ROLE_CLIENT')")
     @PostMapping("/fetchPaymentIntent")
     public ResponseEntity<StripeClientSecretDTO> processPayment(@RequestBody Map<String, String> json) throws JsonProcessingException, StripeException {
         ObjectMapper objectMapper = new ObjectMapper();
@@ -52,6 +55,8 @@ public class StripeController {
         String restaurentStripeAccount =json.get("restaurentStripeAccount");
         return ResponseEntity.ok(stripeService.processPayment(restaurentStripeAccount, BillDTOToBill.instance.convert(billDTO)));
     }
+
+    @PreAuthorize("hasAnyAuthority('ROLE_GUEST','ROLE_CLIENT')")
     @PostMapping("/fetchPaymentRquestPaymentIntent")
     public ResponseEntity<StripeClientSecretDTO> processPaymentRequestPayment(@RequestBody Map<String, String> json) throws JsonProcessingException, StripeException {
         ObjectMapper objectMapper = new ObjectMapper();
@@ -61,36 +66,45 @@ public class StripeController {
         return ResponseEntity.ok(stripeService.processRequestPayment(restaurentStripeAccount, BillDTOToBill.instance.convert(billDTO)));
     }
 
+    @PreAuthorize("hasAuthority('ROLE_OWNER')")
     @GetMapping("/fetchSubscriptionProducts")
     public ResponseEntity<List<StripeSubscriptionProductsDTO>> fetchSubscriptionProducts(){
         return ResponseEntity.ok(stripeService.fetchSubscriptionProducts());
     }
+
+    @PreAuthorize("hasAuthority('ROLE_OWNER')")
     @PostMapping("/fetchSubscriptionSession")
     public ResponseEntity<StripeSessionCustomerIdDTO> processSubscriptionSession (@RequestBody Map <String ,String > json) throws JsonProcessingException, StripeException  {
         String ownerEmail = json.get("ownerEmail");
         return ResponseEntity.ok(stripeService.createCheckoutSession(ownerEmail));
     }
+
+    @PreAuthorize("hasAuthority('ROLE_OWNER')")
     @PostMapping("/createSubscription")
     public ResponseEntity<SubscriptionEntityDTO> createSubscription(@RequestBody SubscriptionRequestDTO subscriptionRequestDTO) throws StripeException, JSONException {
         return ResponseEntity.ok(stripeService.createSubscription(subscriptionRequestDTO));
     }
+
     //permet de mettre a jour la methode de payment si jamais la methode de payment fail;
+    @PreAuthorize("hasAuthority('ROLE_OWNER')")
     @PostMapping("/retrySubscription")
     public ResponseEntity<SubscriptionEntityDTO> retrySubscriptionPaymentMethod(@RequestBody SubscriptionRequestDTO subscriptionRequestDTO) throws Exception {
         return ResponseEntity.ok(stripeService.retryInvoice(subscriptionRequestDTO));
     }
 
-
+    @PreAuthorize("hasAuthority('ROLE_OWNER')")
     @PostMapping("/cancelSubscription")
     public ResponseEntity<SubscriptionEntityDTO> cancelSubscription(@RequestBody RetreiveSubscriptionRequestDTO retreiveSubscriptionRequestDTO) throws StripeException {
         return ResponseEntity.ok(stripeService.cancelSubscription(retreiveSubscriptionRequestDTO.getOwnerEmail()));
     }
 
+    @PreAuthorize("hasAnyAuthority('ROLE_OWNER','ROLE_WAITER','ROLE_COOK')")
     @PostMapping("/retreiveSubscription")
     public ResponseEntity<SubscriptionEntityDTO> retreiveSubscription(@RequestBody RetreiveSubscriptionRequestDTO retreiveSubscriptionRequestDTO) throws StripeException, JSONException {
         return ResponseEntity.ok(stripeService.retreiveSubscription(retreiveSubscriptionRequestDTO));
     }
 
+    @PreAuthorize("hasAuthority('ROLE_OWNER')")
     @GetMapping ("/.well-known/apple-developer-merchantid-domain-association")
     public ResponseEntity<?> returnDomainFile() throws IOException {
         return ResponseEntity.ok(stripeService.returnDomainFile());
