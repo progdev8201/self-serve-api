45c88a1625c0ec077d8e4c4448ca3ccf3eb35fe4
momolakeit <56810449+momolakeit@users.noreply.github.com>
2020-12-23 19:36:20 -0500

    fixProbleme edit retaurant while admin

--- src/main/java/com/service/validator/RestaurantOwnerShipValidator.java
+++ src/main/java/com/service/validator/RestaurantOwnerShipValidator.java
@@ -11,6 +11,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class RestaurantOwnerShipValidator {
 
@@ -23,44 +25,46 @@ public class RestaurantOwnerShipValidator {
     @Autowired
     private AdminRepository adminRepository;
 
-    public boolean hasOwnerRight(Long restauranId){
+    public boolean hasOwnerRight(Long restauranId) {
         Authentication authentication = getAuthentication();
 
         if (authentication == null)
             return true;
 
-        Long ownerId =  Long.parseLong((String) getAuthentication().getPrincipal());
+        Long ownerId = Long.parseLong((String) getAuthentication().getPrincipal());
 
-        Owner owner = ownerRepository.findById(ownerId).get();
-
-        return owner.getRestaurantList().stream().anyMatch(restaurant -> restauranId.equals(restaurant.getId()));
+        Owner owner = ownerRepository.findById(ownerId).orElse(null);
+        if (Objects.nonNull(owner)) {
+            owner.getRestaurantList().stream().anyMatch(restaurant -> restauranId.equals(restaurant.getId()));
+        }
+        return false;
     }
 
-    public boolean isAdminConnected(){
+    public boolean isAdminConnected() {
         Authentication authentication = getAuthentication();
 
         if (authentication == null)
             return true;
 
-        Long adminId =  Long.parseLong((String) getAuthentication().getPrincipal());
+        Long adminId = Long.parseLong((String) getAuthentication().getPrincipal());
 
         return adminRepository.findById(adminId).get().getRole().equals(RoleName.ROLE_ADMIN.toString());
     }
 
-    public boolean hasCookWaiterRight(Long restaurantId){
+    public boolean hasCookWaiterRight(Long restaurantId) {
         Authentication authentication = getAuthentication();
 
         if (authentication == null)
             return true;
 
-        Long employerId =  Long.parseLong((String) getAuthentication().getPrincipal());
+        Long employerId = Long.parseLong((String) getAuthentication().getPrincipal());
 
         Employer employer = employerRepository.findById(employerId).get();
 
         return employer.getRestaurant().getId().equals(restaurantId);
     }
 
-    private Authentication getAuthentication(){
+    private Authentication getAuthentication() {
         return SecurityContextHolder.getContext().getAuthentication();
     }
 }
